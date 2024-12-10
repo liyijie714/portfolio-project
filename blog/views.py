e44180f94667e7c354966b913f71671212dc5e1c
yijie-c <yijie.li@chime.com>
2024-12-10 14:47:48 -0800

    all blogs

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
+
+def allblogs(request):
+    # return render(request, 'blog/allblogs.html')
+    blogs = Blog.objects
+    print(blogs)
+    return render(request, 'blog/allblogs.html', {'blogs': blogs})
