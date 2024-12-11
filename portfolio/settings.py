88c2677a5df2d9428b68388b752b91b7d450e789
yijie-c <yijie.li@chime.com>
2024-12-11 14:51:17 -0800

    blog and polish

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -120,8 +120,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATICFILES_DIRS = [
+    BASE_DIR / 'portfolio/static'
+]
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
